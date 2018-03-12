e735aed08057f7e6a187a2a55799dfa3d1e71776
WarrenF <warren.fitzpatrick@holidayextras.com>
2018-03-12 16:02:49 +0000

    Updated preview and git ignored package-lock file

--- preview/index.js
+++ preview/index.js
@@ -2,7 +2,7 @@
 
 var React = require('react')
 var ReactDOM = require('react-dom')
-var FormBuilder = require('../index').default
+var FormBuilder = require('../app/index').default
 var props = require('../config/previewProps')
 
 ReactDOM.render(<FormBuilder {...props} />, document.getElementById('root'))
