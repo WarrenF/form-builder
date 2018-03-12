d8060ec431afcfd0cac7601f878cdb812ac3abcb
WarrenF <warren.fitzpatrick@holidayextras.com>
2018-03-12 21:43:20 +0000

    added linting

--- app/index.js
+++ app/index.js
@@ -5,10 +5,6 @@ import Input from './components/input'
 import { object, func, string } from 'prop-types'
 
 class FormBuilder extends Component {
-  constructor (props) {
-    super(props)
-  }
-
   buildInputs () {
     const {formSettings, inputClass} = this.props
     const {inputs} = formSettings
